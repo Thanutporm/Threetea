5f29faf58419c9671615b7de297a6ee27d22452c
punyabrt <punya_brt@hotmail.com>
2018-12-14 18:57:42 +0700

    New

--- app/src/main/java/com/example/punyabrt/threetea/addmenu.java
+++ app/src/main/java/com/example/punyabrt/threetea/addmenu.java
@@ -1,7 +1,10 @@
 package com.example.punyabrt.threetea;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class addmenu extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class addmenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addmenu);
+
+        Button back = findViewById(R.id.backTeaIn_button);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(addmenu.this, showdata.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/punyabrt/threetea/showdata.java
+++ app/src/main/java/com/example/punyabrt/threetea/showdata.java
@@ -1,10 +1,13 @@
 package com.example.punyabrt.threetea;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.punyabrt.threetea.Databasetea.TeaIn;
 
 public class showdata extends AppCompatActivity {
 
@@ -13,11 +16,35 @@ public class showdata extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_showdata);
 
+        TeaIn mHelper = new TeaIn(this);
+        SQLiteDatabase mDb = mHelper.getWritableDatabase();
+        mHelper.close();
+        mDb.close();
+
+        Button view = findViewById(R.id.VIEW_button);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(showdata.this,viewmenu.class);
+                startActivity(i);
+            }
+        });
+
         Button add = findViewById(R.id.ADD_button);
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(showdata.this,addmenu.class);
+                startActivity(i);
+            }
+        });
+
+
+        Button edit = findViewById(R.id.EDIT_button);
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(showdata.this,editmenu.class);
                 startActivity(i);
             }
         });
